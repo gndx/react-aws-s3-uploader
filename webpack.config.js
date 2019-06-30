d2b9608dcd199a22e1f5355c6de2c2dd843bdfff
Oscar Barajas <oscar@dragons.mx>
2019-06-30 02:30:34 -0500

    [ADD] Webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'PropTypes',
+      root: 'PropTypes',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'prop-types': path.resolve(__dirname, './node_modules/prop-types'),
+    },
+  },
+}
